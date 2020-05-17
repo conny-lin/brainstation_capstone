ecec31387c283d4a19baed61db4a7a8aabba5653
Conny Lin <connylin318@gmail.com>
2020-05-17 14:47:48 -0700

    initial commit to run chormaster for drunkposture2.dat output

---
+++ 1_database/run_choremaster_drunkposture2_missingfiles.m
@@ -0,0 +1,25 @@
+% run choreography to get drunkposture2.dat output for the 272 files
+% missing this output
+% -------------------------------------------------------------------------
+% Conny Lin | May 19, 2020
+% -------------------------------------------------------------------------
+% add path to chormaster function and general matlab local functions
+addpath(genpath('/Users/connylin/Dropbox/Code/language/matlab_general'))
+addpath(genpath('/Users/connylin/Dropbox/Code/proj/rankin_lab'))
+% load csv to get mwt paths
+p = '/Users/connylin/Dropbox/CA/ED _20200119 Brain Station Data Science Diploma/Capstone/data/path_mwt_no_drunkposture2.csv';
+T = readtable(p,'Delimiter','comma');
+% get paths into cell array
+pMWTS = T.mwtpath;
+%%
+[p,fn] = fileparts(pMWTS{1});
+
+%% run chore for drunkposture2 on 1 file to test
+for i = 1:numel(pMWTS)
+    fprintf('processing: %s',pMWTS{i})
+    [~,pMWTpass,pMWTfailed] = chormaster5('DrunkPosture',pMWTS(i));
+end
+
+
+%% run on all
+[Legend,pMWTpass,pMWTfailed] = chormaster5('DrunkPosture',pMWTS);

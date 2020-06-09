be9775fecf754e5fada18e90b02da862898ff7cf
Conny Lin <connylin318@gmail.com>
2020-06-09 10:18:26 -0700

    commit yesterday's work

--- 0_lib/BrainStationLib.py
+++ 0_lib/BrainStationLib.py
@@ -4,6 +4,8 @@
 import os, sys, socket, time, re, glob, pickle
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # system handling ====================================================================
 def getcomputername():
@@ -306,5 +308,29 @@ def nutcracker_split_Xy(data, dir_save):
 
 # end -- Data processing ============================================================
 
+# Visualization  ====================================================================
+def heatmap_corr(cor):
+    cmap = sns.diverging_palette(220, 10, as_cmap=True)
+    # create mask
+    i = cor<0
+    corr_positive = cor.copy()
+    corr_positive[i] = cor[i]*-1
+    corr_positive[~i] = cor[~i]*1
+    mask = np.triu(np.ones_like(corr_positive, dtype=np.bool))
+    # plot figure
+    plt.figure()
+    f, ax = plt.subplots(figsize=(11, 9))
+    sns.heatmap(cor, square=True,mask=mask,cmap=cmap,
+                linewidths=.5, ax=ax)
+    plt.title('rho')
+    plt.show()
+
+def plothist_2groups(X_no, X_etoh, bins, feature_name):
+    plt.hist(X_no, bins, alpha=0.5, label='no ethanol')
+    plt.hist(X_etoh, bins, alpha=0.5, label='ethanol')
+    plt.title(feature_name)
+    plt.legend(loc='upper right')
+    plt.show()
+# end --- Visualization  ============================================================
 
 

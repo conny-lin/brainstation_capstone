6dd963f00704f6cc4d5b0c702aac733a1f192d4b
Conny Lin <connylin318@gmail.com>
2020-06-13 17:09:57 -0700

    all changes commit

--- BrainStationLib/BrainStationLibCollect.py
+++ BrainStationLib/BrainStationLibCollect.py
@@ -325,12 +325,12 @@ def heatmap_corr(cor):
     plt.title('rho')
     plt.show()
 
-def plothist_2groups(X_no, X_etoh, bins, feature_name):
-    plt.hist(X_no, bins, alpha=0.5, label='no ethanol')
-    plt.hist(X_etoh, bins, alpha=0.5, label='ethanol')
-    plt.title(feature_name)
-    plt.legend(loc='upper right')
-    plt.show()
+# def plothist_2groups(X_no, X_etoh, bins, feature_name):
+#     plt.hist(X_no, bins, alpha=0.5, label='no ethanol')
+#     plt.hist(X_etoh, bins, alpha=0.5, label='ethanol')
+#     plt.title(feature_name)
+#     plt.legend(loc='upper right')
+#     plt.show()
 # end --- Visualization  ============================================================
 
 

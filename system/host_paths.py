1f6d043ed99deae1f980dd687643bfcc0cc3a460
Conny Lin <connylin318@gmail.com>
2020-06-15 12:23:37 -0700

    fix missing dash between Angular-Gyrus

--- system/host_paths.py
+++ system/host_paths.py
@@ -8,7 +8,7 @@ def get(hostname):
         localpath = {
             'Capstone':'/Users/connylin/Dropbox/CA/ED _20200119 Brain Station Data Science Diploma/Capstone',
             'datapath':'/Users/connylin/Dropbox/CA/ED _20200119 Brain Station Data Science Diploma/Capstone/data/nutcracker_sample_1Meach.csv'}
-    elif hostname == 'Angular Gyrus':
+    elif hostname == 'Angular-Gyrus':
         localpath = {
             'Capstone':'/Users/connylin/Dropbox/CA/ED _20200119 Brain Station Data Science Diploma/Capstone',
             'datapath':'/Users/connylin/Dropbox/CA/ED _20200119 Brain Station Data Science Diploma/Capstone/data/nutcracker_sample_1Meach.csv'}

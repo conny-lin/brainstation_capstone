57ae060fe135553a6afe626b758dab1cbb73305a
Conny Lin <connylin318@gmail.com>
2020-06-18 13:54:06 -0700

    updated loaddata with print statements

--- etl/loaddata.py
+++ etl/loaddata.py
@@ -10,11 +10,13 @@ def nutcracker(localpaths, dataname, datatype):
 
     """
     dir_datafolder = os.path.join(localpaths['Capstone'], 'data')
-
+    print(f'loading {len(datatype)} files')
     datadict = dict()
-    for dname in datatype:
+    for i, dname in enumerate(datatype):
+        print(f'loading file: {i}', end='\r')
         filename = dataname + '_' + dname + '.csv'
         filepath = os.path.join(dir_datafolder, filename)
         data = pd.read_csv(filepath, header=None, index_col=False)
         datadict[dname] = data.to_numpy()
+    print('loading completed')
     return datadict

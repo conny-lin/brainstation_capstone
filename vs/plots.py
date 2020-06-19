72c72a5e8a6a255755b172b7b9595a6e4e35c044
Conny Lin <connylin318@gmail.com>
2020-06-19 15:06:09 -0700

    support code

--- vs/plots.py
+++ vs/plots.py
@@ -4,11 +4,19 @@ import matplotlib.pyplot as plt
 
 def hyperparameterplot(hyperparameter_list, train_score_list, test_score_list, \
                         hyperparameter_name='', titlename=''):
+    
+    # check if hyperparameter list is string or number
+    if isinstance(hyperparameter_list[0], str):
+        hyperparameter_label = hyperparameter_list.copy()
+        hyperparameter_list = range(len(hyperparameter_list))
+    # graph
     plt.figure()
     plt.plot(hyperparameter_list, train_score_list, color='blue', label='train')
     plt.plot(hyperparameter_list, test_score_list, color='red', label='test')
     plt.title(titlename)
     plt.xlabel(hyperparameter_name)
+    if isinstance(hyperparameter_list[0], str):
+        plt.xticks(labels=hyperparameter_label)
     plt.ylabel('score')
     plt.legend()
     plt.show()

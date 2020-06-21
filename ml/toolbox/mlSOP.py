42bfb10bd1ae386e92b8116645e6b811dd744b84
Conny Lin <connylin318@gmail.com>
2020-06-20 17:40:34 -0700

    add gridcv object

--- ml/toolbox/mlSOP.py
+++ ml/toolbox/mlSOP.py
@@ -1,5 +1,7 @@
 import numpy as np
 import time
+from sklearn.model_selection import GridSearchCV
+
 
 class test_model:
     def __init__(self):
@@ -65,4 +67,34 @@ class ml_timer:
         return self.session_times
     
 
+class GridSearchCVHelper:
+    def __init__(self, model, grid):
+        self.grid = grid
+        self.model = model
+    
+    def run(self, X, y, **kwargs):
+        # process kwargs
+        n_jobs = kwargs.pop('n_jobs', -1)
+        cv = kwargs.pop('cv', 3)
+        scoring = kwargs.pop('scoring', 'accuracy')
+        error_score = kwargs.pop('error_score', 0)
+        # instantiate grid search
+        self.grid_search = GridSearchCV(estimator=self.model, 
+                                param_grid=self.grid,
+                                n_jobs=n_jobs, 
+                                cv=cv, scoring=scoring, 
+                                error_score=error_score)
+        self.grid_result = self.grid_search.fit(X, y)
+        return self.grid_result, self.grid_search
+    
+    def print_result(self):
+        print("Best: %f using %s" % (self.grid_result.best_score_, 
+                                    self.grid_result.best_params_))
+        means = self.grid_result.cv_results_['mean_test_score']
+        stds = self.grid_result.cv_results_['std_test_score']
+        params = self.grid_result.cv_results_['params']
+        for mean, stdev, param in zip(means, stds, params):
+            print("%f (%f) with: %r" % (mean, stdev, param))
+        return means, stds, params
+    
     
